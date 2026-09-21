76fd37ec2bdc6d750ca2526df8c45cbfb441e970
Denys Dovhan <denysdovhan@gmail.com>
2026-09-21 19:12:21 +0300

    fix: hide battery section when data is unavailable

--- src/battery.sh
+++ src/battery.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # macOS battery percentage and background color, emitted together to stay in sync.
-pmset -g batt | awk -v icon="$1" -v low="$2" -v medium="$3" -v high="$4" -v full="$5" -v charging="$6" '
+pmset -g batt 2>/dev/null | awk -v icon="$1" -v low="$2" -v medium="$3" -v high="$4" -v full="$5" -v charging="$6" -v separator="$7" '
   match($0, /[0-9]+%/) {
     level = substr($0, RSTART, RLENGTH) + 0
     colour = full
@@ -8,7 +8,7 @@ pmset -g batt | awk -v icon="$1" -v low="$2" -v medium="$3" -v high="$4" -v full
     if (level < 25) colour = medium
     if (level < 10) colour = low
     if ($0 ~ /; charging;/) icon = charging
-    printf "#[bg=%s] %s %d%%\n", colour, icon, level
+    printf "#[bg=%s] %s %d%%%s\n", colour, icon, level, separator
     exit
   }
 '
